cee80e7ef6c0e76e64a08da604ea0c792c94dd65
Ciaran McCrisken <ciaran.mccrisken@puppet.com>
2020-06-17 22:06:30 +0100

    Add cut off date limit

--- bin/gh_pr_query/gh_pr_query.rb
+++ bin/gh_pr_query/gh_pr_query.rb
@@ -2,6 +2,7 @@
 # frozen_string_literal: true
 
 INTERESTED_FILE = '.github/workflows'
+DATE_TIME_CUT_OFF = 1561982400 # 2020-07-01 12:00:00 UTC
 
 raise 'Please set Github API token in ENV VAR $GITHUB_TOKEN' unless ENV['GITHUB_TOKEN']
 
@@ -9,6 +10,7 @@ require_relative 'octokit_utils'
 require 'json'
 
 MANAGED_MODULES_URI = 'https://puppetlabs.github.io/iac/modules.json'
+
 uri = URI.parse(MANAGED_MODULES_URI)
 response = Net::HTTP.get_response(uri)
 output = response.body
@@ -19,13 +21,19 @@ client = util.client
 
 repos.each do |_k, v|
   repo_name = v['title']
-  puts "Querying the following PRs in #{repo_name}:"
+  puts "\nQuerying the following PRs in #{repo_name}:"
   page_num = 1
   suspect_prs = []
   pr_res = client.get("repos/puppetlabs/#{repo_name}/pulls", state: 'all', page: page_num)
-  until pr_res.empty? do
+  pr_max_date = false
+  until pr_res.empty? or pr_max_date do
     pr_res.each do |pr|
       pr_number = pr['number']
+      if pr[:created_at].to_i < DATE_TIME_CUT_OFF
+        puts "\n=== PR ##{pr_number} was created before 2020-07-01 12:00:00 - no more PRs will be processed for #{repo_name} ===\n"
+        pr_max_date = true
+      end
+      break if pr_max_date
       puts "##{pr_number}: "
       files_changed = client.get("/repos/puppetlabs/#{repo_name}/pulls/#{pr_number}/files")
       files_changed.each do |file|
